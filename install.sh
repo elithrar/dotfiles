7617d4092dac1582434ca28dccaa19080bcd1952
Matt Silverlock <matt@eatsleeprepeat.net>
2020-04-03 20:36:59 -0700

    install: add Alfred App

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 # Configuration
 DOTFILES_REPO="https://github.com/elithrar/dotfiles"
 BREW_PACKAGES=(asciinema cmake curl fd gifski git go htop jq jupyter lua make neovim nmap node python reattach-to-user-namespace rcm ripgrep tmux tree wget wrk yarn youtube-dl zsh)
-CASKS=(1password dropbox font-fira-code rectangle spotify)
+CASKS=(1password alfred dropbox font-fira-code rectangle spotify)
 SSH_EMAIL="matt@eatsleeprepeat.net"
 
 # Colors
