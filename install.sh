185ee94e827fab661d97c8ab215f7c1840744b9a
Matt Silverlock <matt@eatsleeprepeat.net>
2025-12-12 07:55:44 -0500

    status bar parent/cwd windows

--- install.sh
+++ install.sh
@@ -9,7 +9,6 @@ DOTFILES_REPO="https://github.com/elithrar/dotfiles"
 BREW_PACKAGES=(age asciinema atuin bat bun cmake curl delta fd gifski git go htop jq lua make mkcert neovim nmap node pipx python rcm ripgrep tmux tree websocat wget wrk yarn zsh cloudflare/cloudflare/cloudflared stow rbenv ruby-build uv ruff gh shellcheck fzf zoxide)
 CASKS=()
 SSH_EMAIL="matt@eatsleeprepeat.net"
-CLOUDSDK_INSTALL_DIR="${HOME}/repos"
 
 # Colors
 reset="$(tput sgr0)"
